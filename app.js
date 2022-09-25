85aebc48b99142a64a6b06ccd6605267dfe6b06b
Thomas Salin <salin.thomas.pro@gmail.com>
2022-09-25 11:11:02 +0200

    init git (Front CDSModel3D)

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(__dirname + '/src'))
+
+app.use('/three/', express.static(path.join(__dirname,'/three')));
+
+
+app.listen(3000, () =>{
+    console.log("http://localhost:3000");
+} )
---
+++ src/tool.js
@@ -0,0 +1,65 @@
+import * as THREE from '/three/three.module.js'
+import { OrbitControls } from '/three/OrbitControls.js'
+import { STLLoader } from '/three/STLLoader.js'
+
+const colorScene = new THREE.Color(0xF0F0F2);
+
+
+const scene = new THREE.Scene({
+    background: colorScene,
+});
+
+const light = new THREE.DirectionalLight( 0xffffff, 1 );
+light.position.set( 100, 100, 100 ); //default; light shining from top
+light.castShadow = false; // default false
+scene.add( light );
+
+const light1 = new THREE.DirectionalLight( 0xffffff, 1 );
+light1.position.set( -100, -100, -100 ); //default; light shining from top
+light1.castShadow = false; // default false
+scene.add( light1 );
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const material = new THREE.MeshPhongMaterial({
+    color: 0xF0F0F2,
+    emissive: 0x000000,
+    specular: 0x111111,
+    shininess: 30,
+    reflectivity: 1.0,
+    refractionRatio: 0.98,
+})
+
+
+const loader = new STLLoader();
+loader.load('models/Bracket.stl',
+            function (geometry){
+                const mesh = new THREE.Mesh(geometry, material)
+                scene.add(mesh)
+                },
+                () => {
+                    console.log("loaded")
+                },
+                (error) => {
+                    console.log(error);
+                }
+)
+
+camera.position.set(0,100,200)
+controls.update();
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    controls.update();
+    
+    renderer.render( scene, camera );
+};
+
+animate();
